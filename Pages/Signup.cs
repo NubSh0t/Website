a02e97fd73a2f1ba8f2553b7662a56e3532bb2ee
NubSh0t <1ammarahmed23@gmail.com>
2025-05-06 05:13:23 +0500

    yes

--- Pages/Signup.cs
+++ Pages/Signup.cs
@@ -32,7 +32,11 @@ public class SignupModel : PageModel
         }
         string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "logins.txt");
 
-        Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+        if (!System.IO.File.Exists(filePath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+        }
+
 
         string line = $"FullName: {FullName}, Email: {Email}, Password: {Password}";
         System.IO.File.AppendAllText(filePath, line + Environment.NewLine);
